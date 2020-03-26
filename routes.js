7265a06988a4a5a82ea6af133af745427adac090
dd-tech-nz <ddumolo@hotmail.com>
2020-03-26 21:16:57 +1300

    create data.json and add images files

---
+++ routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const routes = express.Router()
+
+module.exports = routes
+
+// GET
+routes.get('/foo', (req, res) => {
+  res.send('Testing....Foo!')
+})
